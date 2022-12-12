ed2ccbd4de1fc43f646b741a9188bfafea52b621
zainzaidanahsan <ahsanzidan60@gmail.com>
2022-12-12 19:47:48 +0700

    tampilan flight crud -zain push

---
+++ app/src/main/java/com/ctwofinalproject/ticketing/admin/view/ui/flight/SelectOneWayFragment.kt
@@ -0,0 +1,20 @@
+package com.ctwofinalproject.ticketing.admin.view.ui.flight
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ctwofinalproject.ticketing.admin.R
+
+class SelectOneWayFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_select_one_way, container, false)
+    }
+
+}
