59e5820a13bb336ff11d8ba8ba22864b526d5a41
zainzaidanahsan <ahsanzidan60@gmail.com>
2022-12-16 13:19:57 +0700

    Flight detail update 2

--- app/src/main/java/com/ctwofinalproject/ticketing/admin/view/ui/flight/DetailFlightFragment.kt
+++ app/src/main/java/com/ctwofinalproject/ticketing/admin/view/ui/flight/DetailFlightFragment.kt
@@ -1,5 +1,6 @@
 package com.ctwofinalproject.ticketing.admin.view.ui.flight
 
+import android.app.TimePickerDialog
 import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TimePicker
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import com.ctwofinalproject.ticketing.admin.R
@@ -17,13 +19,23 @@ import com.ctwofinalproject.ticketing.admin.databinding.FragmentDetailFlightBind
 import com.ctwofinalproject.ticketing.admin.databinding.FragmentShowListFlightBinding
 import com.ctwofinalproject.ticketing.admin.viewmodel.DetailFlightViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 
 @AndroidEntryPoint
-class DetailFlightFragment : Fragment() {
+class DetailFlightFragment : Fragment(), TimePickerDialog.OnTimeSetListener {
     private var _binding : FragmentDetailFlightBinding?                                  = null
     private val binding get()                                                            = _binding!!
     var idFlight                                                                         = ""
     val detailFlightViewModel                                                            : DetailFlightViewModel by viewModels()
+
+    var hour = 0
+    var minute = 0
+
+    var savedHour = 0
+    var savedMinute = 0
+
+    var selectTime = ""
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,6 +58,20 @@ class DetailFlightFragment : Fragment() {
             ivGotoBackFromFDetailFlight.setOnClickListener {
                 Navigation.findNavController(binding.root).popBackStack()
             }
+
+
+            iconTimeDepature.setOnClickListener{
+                selectTime = "depature"
+                getDateTimePicker()
+                TimePickerDialog(requireContext(),this@DetailFlightFragment,hour,minute,true).show()
+            }
+
+            iconTimeArrival.setOnClickListener{
+                selectTime = "arrival"
+                getDateTimePicker()
+                TimePickerDialog(requireContext(),this@DetailFlightFragment,hour,minute,true).show()
+            }
+
             btnCreateFlight.setOnClickListener{
                 detailFlightViewModel.updateFlight(FlightBody(Flight(txtDepature.toString(),1,txtArrivalTime.toString(),
                     1,tvDateDepartureDateFragmentSelectOneWay.toString(),1,2),
@@ -54,6 +80,11 @@ class DetailFlightFragment : Fragment() {
             }
 
         }
+    }
+    private fun getDateTimePicker(){
+        val cal = Calendar.getInstance()
+        hour = cal.get(Calendar.HOUR)
+        minute = cal.get(Calendar.MINUTE)
     }
 
     private fun getArgs() {
@@ -67,5 +98,16 @@ class DetailFlightFragment : Fragment() {
         binding.txtArrival.text = arguments?.getString("arrivalTime","")
 
         idFlight = arguments?.getInt("idFlight",0).toString()
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        savedHour = hourOfDay
+        savedMinute = minute
+
+        if (selectTime.equals("arrival")){
+            binding.txtArrivalTime.text = "$savedHour:$savedMinute:00"
+        }else{
+            binding.txtDepatureTime.text = "$savedHour:$savedMinute:00"
+        }
     }
 }
